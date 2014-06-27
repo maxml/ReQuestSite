d1be51bd3ce70f71a3e69296058f7c1ac22327af
maxml <maxml@inbox.ru>
2014-06-27 17:50:09 +0300

    google registration, parse bug

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,176 +1,107 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 Parse.initialize("V10TgoAKTJ7B8H8YjJhgucaXdGiDeROgxACn6aA2", "1gGbFOhUUrgeVp7JkqLP4XkOc8mBWkrQCU1uKAi8");
+var uname, upic, uemail, u, counter = 0;
 
-
-function signinCallback(authResult) {
-    $('#loader').show();
-
-    if (authResult['access_token']) {
-
-        gapi.auth.setToken(authResult);
-        gapi.client.load('plus', 'v1', function() {
-            getName();
-        });
-
-        getEmail();
-
-        $('.info').css("display", "block");
-        $('#signinButton').css('display', 'none');
-
-        $('#email').trigger('contentchanged');
-        $('#name').trigger('contentchanged');
-        $('#pic').trigger('contentchanged');
-
-    } else if (authResult[ 'error' ]) {
-        console.log('Sign-in state: ' + authResult[ 'error' ]);
-    }
-
-    $('#loader').hide();
-}
-
-function disconnectUser(access_token) {
-    var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + access_token;
-
-    $.ajax({
-        type: 'GET',
-        url: revokeUrl,
-        async: false,
-        contentType: "application/json",
-        dataType: 'jsonp',
-        success: function(nullResponse) {
-            alert('Вы вышли из системы! Ждем Вас еще!');
-        },
-        error: function(e) {
-            console.log(e);
-        }
-    });
-}
-
-function getName() {
-
-    var request = gapi.client.plus.people.get({'userId': 'me'});
-    request.execute(function(resp) {
-        var username = String(resp.displayName).split(' ');
-        $('#name').text(username[0] + ' ' + username[1]);
-        $('#pic').html('<img id="user_photo" src="' + resp['image']['url'] + '" />');
-    });
-}
-
-function getEmail() {
-
-    gapi.client.load('oauth2', 'v2', function() {
-        var request = gapi.client.oauth2.userinfo.get({'userId': 'me'});
-        request.execute(function(resp) {
-            $('#email').text(resp['email']);
-        });
-    });
-}
-
-$('#revoke').click(function() {
-
-    $('#loader').show();
-    disconnectUser(gapi.auth.getToken().access_token);
-    $('#loader').hide();
+function logout()
+{
+    gapi.auth.signOut();
 
     $('.info').css("display", "none");
     $('#signinButton').show();
 
-});
+//    location.reload();
+}
+function login()
+{
+    var myParams = {
+        'clientid': '543719723243-g5d4k6aic1ugr5rf506t9rh2ohptgsf3.apps.googleusercontent.com',
+        'cookiepolicy': 'single_host_origin',
+        'callback': 'loginCallback',
+        'approvalprompt': 'force',
+        'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+    };
+    gapi.auth.signIn(myParams);
+}
 
-function userToParse() {
-    if (findUser() !== '') {
-//        console.log("Authorised!");
-        return;
+function loginCallback(result)
+{
+    if (result['status']['signed_in'])
+    {
+        var request = gapi.client.plus.people.get({
+            'userId': 'me'
+        });
+        request.execute(function(resp)
+        {
+            var email = '';
+            if (resp['emails'])
+            {
+                for (i = 0; i < resp['emails'].length; i++)
+                {
+                    if (resp['emails'][i]['type'] == 'account')
+                    {
+                        email = resp['emails'][i]['value'];
+                    }
+                }
+            }
+
+            uname = resp['displayName'];
+//            console.log("Name " + uname);
+
+            upic = resp['image']['url'];
+//            console.log("Pic " + upic);
+
+            uemail = email;
+//            console.log("Email " + uemail);
+
+            $('#email').text(uemail);
+            $('#name').text(uname);
+            $('#pic').html('<img id="user_photo" src="' + upic + '" />');
+
+            $('.info').css("display", "block");
+            $('#signinButton').css('display', 'none');
+
+            toParse();
+        });
     }
 
+}
+function onLoadCallback()
+{
+    //gapi.client.setApiKey('AIzaSyAHtIQcOBrfA-OYZXYxfnPKvG8bL_gfa_U');
+    gapi.client.load('plus', 'v1', function() {
+    });
+}
+
+function toParse() {
     var User = Parse.Object.extend("User");
     var Wallet = Parse.Object.extend("Wallet");
 
     // Create a new instance of that class.
-    var wallet = new Wallet();
-    wallet.set("total", 3);
-
     var user = new User();
+//    console.log("userToParse " + uname + uemail + upic);
 
     user.set("username", uname);
     user.set("email", uemail);
     user.set("avatar", upic);
+
     user.set("password", "my-pass");
-    user.set("wallet", wallet);
 
-    user.save();
-    wallet.save();
-//    console.log("SAVE!");
+    user.signUp(null, {
+        success: function(user) {
 
-    findUser();
-}
+            var wallet = new Wallet();
+            wallet.set("total", 0);
 
-var uname, upic, uemail, u, counter = 0;
-$(document).ready(function() {
-
-    $('#name').bind('contentchanged', function() {
-        // do something after the div content has changed
-        setTimeout(function() {
-            uname = String($('#name').text()).substring(5);
-//            console.log(uname);
-
-            agregateUserInfo();
-        }, 3000);
-    });
-
-    $('#pic').bind('contentchanged', function() {
-        // do something after the div content has changed
-        setTimeout(function() {
-            upic = String($('#user_photo').prop('src'));
-//            console.log(upic);
-
-            agregateUserInfo();
-        }, 3000);
-    });
-
-    $('#email').bind('contentchanged', function() {
-        // do something after the div content has changed
-        setTimeout(function() {
-            uemail = String($('#email').text()).split(' ')[1];
-//            console.log(uemail);
-
-            agregateUserInfo();
-        }, 3000);
-    });
-
-    function agregateUserInfo() {
-        counter++;
-//        console.log(counter);
-
-        if (counter === 3) {
-            counter = 0;
-            userToParse();
-        }
-    }
-});
-
-function findUser() {
-    var User = Parse.Object.extend("User");
-
-    var query = new Parse.Query(User);
-    query.equalTo("email", uemail);
-
-    query.find({
-        success: function(results) {
-            // Do something with the returned Parse.Object values
-            var object = results[0];
-            u = object;
-
-            return true;
+            user.set('wallet', wallet);
+            user.save();
         },
-        error: function(error) {
-            console.log("Error: " + error.code + " " + error.message);
-            return false;
+        error: function(buff, error) {
+            Parse.User.logIn(uname, "my-pass", {
+                success: function(regUser) {
+                },
+                error: function(buff, error) {
+                    console.log("Auto " + error);
+                }
+            });
         }
     });
 }
