ce969a1c6ccad1fe55366b1a554f7a720af327cb
harker777 <harker6666@gmail.com>
2014-07-10 13:39:18 +0300

    Refactored login / implemented session storage

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,24 @@ app.config(['$routeProvider',
 				redirectTo: 'login'
 			});
 	}
-]);
+]).run(function($rootScope) {
+	Parse.initialize("V10TgoAKTJ7B8H8YjJhgucaXdGiDeROgxACn6aA2", "1gGbFOhUUrgeVp7JkqLP4XkOc8mBWkrQCU1uKAi8");
+
+	User = Parse.Object.extend("User");
+	Wallet = Parse.Object.extend("Wallet");
+
+	console.log($rootScope);
+	// Load user from session if exists
+	var currentParseUser = User.current();
+	if (currentParseUser !== undefined) {
+		var user = {};
+		user.email = currentParseUser.get("username");
+		user.name = currentParseUser.get("name");
+		user.pic = currentParseUser.get("avatar");
+
+		$rootScope.$apply(function() {
+			$rootScope.user = user;
+		});
+	}
+});
+
--- src/js/controller/login.js
+++ src/js/controller/login.js
@@ -1,11 +1,70 @@
 var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('LoginController', ['$scope', '$rootScope', '$q', function($scope, $rootScope, $q) {
+	$scope.login = function() {
+		gapi.auth.signIn({
+			'clientid': '543719723243-g5d4k6aic1ugr5rf506t9rh2ohptgsf3.apps.googleusercontent.com',
+			'cookiepolicy': 'single_host_origin',
+			'callback': 'loginCallback',
+			'approvalprompt': 'force',
+			'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+		});
+	};
+
+	$scope.logout = function() {
+		User.logOut();
+		$rootScope.user = undefined;
+	};
+
 	loginCallback = function(result) {
 		var promise =  extractUser(result);
+		var parseUser = null;
 		promise.then(function(user) {
+			parseUser = convertUserToParseFormat(user);
+			findUser(user.email).then(function(result) {
+				if (!result) {
+					storeUserToParse(convertUserToParseFormat(user));
+				}
+			});
+			parseUser.logIn();
 			$rootScope.user = user;
 		});
+	};
+
+	convertUserToParseFormat = function(rawUser) {
+		var readyToStoreUser = new User();
+
+		readyToStoreUser.set("username", rawUser.email);
+		readyToStoreUser.set("name", rawUser.name);
+		readyToStoreUser.set("avatar", rawUser.pic);
+		readyToStoreUser.set("password", "my-pass");
+
+		return readyToStoreUser;
+	};
+
+	storeUserToParse = function(readyToStoreUser) {
+		readyToStoreUser.signUp(null, {
+			success: function(storedUser) {
+				addWalletToUser(storedUser);
+			}
+		});
+
+		return readyToStoreUser;
+	};
+
+	findUser = function(email) {
+		var deferred = $q.defer();
+		var query = new Parse.Query(User);
+
+		query.equalTo("username", email);
+		return query.first();
+	};
+
+	addWalletToUser = function(parseUser) {
+		var wallet = new Wallet();
+		wallet.set("total", 0);
+		parseUser.set("wallet", wallet);
+		parseUser.save();
 	};
 
 	extractUser = function(result) {
@@ -13,7 +72,7 @@ appControllers.controller('LoginController', ['$scope', '$rootScope', '$q', func
 		var deferred = $q.defer();
 
 		gapi.client.load('plus', 'v1', function() {
-			if (result['status']['signed_in']) {
+			if (result.status.signed_in) {
 				var request = gapi.client.plus.people.get({
 					'userId': 'me'
 				});
@@ -40,18 +99,4 @@ appControllers.controller('LoginController', ['$scope', '$rootScope', '$q', func
 		});
 		return deferred.promise;
 	}
-
-	$scope.login = function() {
-		gapi.auth.signIn({
-			'clientid': '543719723243-g5d4k6aic1ugr5rf506t9rh2ohptgsf3.apps.googleusercontent.com',
-			'cookiepolicy': 'single_host_origin',
-			'callback': 'loginCallback',
-			'approvalprompt': 'force',
-			'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
-		});
-	};
-
-	$scope.logout = function() {
-		$rootScope.user = undefined;
-	};
 }]);
