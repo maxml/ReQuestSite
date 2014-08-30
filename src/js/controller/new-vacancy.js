c6d842ea619963a0b64fea29daf30c2a6c3c446b
harker777 <harker6666@gmail.com>
2014-08-30 17:51:30 +0300

    Implemented basic new vacancy creation

--- src/js/controller/new-vacancy.js
+++ src/js/controller/new-vacancy.js
@@ -2,5 +2,41 @@ appControllers.controller('NewVacancyController', ['$scope', '$rootScope', '$q',
 
     $scope.page = 'first';
 
+    $scope.storeVacancy = function(vacancy) {
+        $scope.vacancy = angular.copy(vacancy);
+    };
+
+    $scope.goToPage = function(page, vacancy) {
+        console.log(vacancy);
+        $scope.storeVacancy(vacancy);
+        $scope.page = page;
+    };
+
+    $scope.sendVacancyToParse = function() {
+        var vacancyEntity = $scope.getEntityFromForm();
+        console.log(vacancyEntity);
+        vacancyEntity.save(null, {
+            success: function() {
+                console.log("success");
+            }
+        });
+    };
+
+    $scope.getEntityFromForm = function() {
+        var result = new Request();
+
+        result.set("city", $scope.vacancy.city);
+        result.set("company", $scope.vacancy.companyName);
+        result.set("company_address", $scope.vacancy.address);
+        result.set("company_description", $scope.vacancy.companyDescription);
+        result.set("demands", $scope.vacancy.demands);
+        result.set("expire", $scope.vacancy.expire);
+        result.set("reward", $scope.vacancy.reward);
+        result.set("terms", $scope.vacancy.terms);
+        result.set("title", $scope.vacancy.name);
+
+        $scope.vacancy = {};
+        return result;
+    };
 }]);
 
